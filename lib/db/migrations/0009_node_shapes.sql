e018813c5533dbd5926df4403425d8b7310dd50f
Charles <cwang633@wcupa.edu>
2026-05-09 04:01:29 -0400

    fix: add statement-breakpoints to migration 0009

--- lib/db/migrations/0009_node_shapes.sql
+++ lib/db/migrations/0009_node_shapes.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `heap_nodes` ADD `shape` text NOT NULL DEFAULT 'rectangle';
+--> statement-breakpoint
 ALTER TABLE `heap_nodes` ADD `width` real;
+--> statement-breakpoint
 ALTER TABLE `heap_nodes` ADD `height` real;
